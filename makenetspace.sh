3c47b323d52c4e55ee461438efb07ce03d0c1650
Malcolm Schongalla <malcolm.schongalla@gmail.com>
2022-03-12 21:54:23 -0500

    copyright updated

--- makenetspace.sh
+++ makenetspace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # makenetspace v 0.2.0-alpha
-# Copyright 2021 Malcolm Schongalla, released under the MIT License (see end of file)
+# Copyright 2021, 2022 Malcolm Schongalla, released under the MIT License (see end of file)
 #
 # malcolm.schongalla@gmail.com
 #
